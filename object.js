73268aa3b594817837bb363412cf00ed5cad8280
arcpy-naf <arcpy.naf@gmail.com>
2020-02-26 23:43:20 +0600

    object

---
+++ object.js
@@ -0,0 +1,7 @@
+var student={id:12, name: "akash", phone:01545};
+var student2={id:13, name:"batash", phone:24456468 };
+
+var phoneNum = student.phone;
+console.log(student);
+console.log(phoneNum);
+console.log(student2);
